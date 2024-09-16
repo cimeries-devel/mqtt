49333cb85decc1f6209b00a96d0f6d0134b32e1c
cimeries <cimeries.devel@gmail.com>
2024-09-16 07:27:53 -0500

    add client for device pi3

--- firebase/client.py
+++ firebase/client.py
@@ -1,18 +1,39 @@
 import threading
+import firebase_admin
+import time
+from firebase_admin import credentials
+from firebase_admin import firestore
+from os.path import join
 
 
-# # Create an Event for notifying main thread.
-# callback_done = threading.Event()
-#
-#
-# # Create a callback on_snapshot function to capture changes
-# def on_snapshot(doc_snapshot, changes, read_time):
-#     for doc in doc_snapshot:
-#         print(f"Received document snapshot: {doc.id}")
-#     callback_done.set()
-#
-#
-# doc_ref = db.collection("cities").document("SF")
-#
-# # Watch the document
-# doc_watch = doc_ref.on_snapshot(on_snapshot)
+class Pi:
+    def __init__(self, config, root):
+        file_key = join(root, config.get('key'))
+        self.cred = credentials.Certificate(file_key)
+        app = firebase_admin.initialize_app(self.cred)
+        self.db = firestore.client(app)
+        self.callback_done = threading.Event()
+
+    def on_snapshot(self, doc_snapshot, changes, read_time):
+        for doc in doc_snapshot:
+            print(doc.to_dict())
+        self.callback_done.set()
+
+    def watch(self):
+        ref = self.db.collection('pi').document('lse01')
+        ref.on_snapshot(self.on_snapshot)
+        while True:
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                break
+
+    def get(self):
+        doc = self.db.collection('pi').document('lse01')
+        data = doc.get()
+        if data.exists:
+            print(data.to_dict())
+        else:
+            print('not data')
+
+
--- pi3.py
+++ pi3.py
@@ -1,5 +1,4 @@
-from client.client import Client
-from firebase.admin import Admin
+from firebase.client import Pi
 from dotenv import dotenv_values
 from os.path import dirname, abspath, join
 
@@ -13,14 +12,5 @@ class App:
 
 if __name__ == '__main__':
     app = App()
-    client = Client(app.config)
-    admin = Admin(app.config, app.root)
-
-    while True:
-        try:
-            client.connect()
-            if client.is_valid_data():
-                break
-            admin.send(client.get_data())
-        except KeyboardInterrupt:
-            break
+    pi = Pi(app.config, app.root)
+    pi.watch()
